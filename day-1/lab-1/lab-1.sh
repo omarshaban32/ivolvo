1a1004e775a44871c29b77cddc2dbe3ffdddefaa
omarshaban32 <54452908+omarshaban32@users.noreply.github.com>
2024-06-09 02:33:50 +0300

    Update lab-1.sh

--- day-1/lab-1/lab-1.sh
+++ day-1/lab-1/lab-1.sh
@@ -2,8 +2,7 @@
 
 sudo adduser webadmin
 sudo usermod -aG webgroup webadmin
-sudo visudo
-%webadmin ALL=(ALL) NOPASSWD: /usr/bin/apt-get install apache2
+sudo visudo               #  %webadmin ALL=(ALL) NOPASSWD: /usr/bin/apt-get install apache2
 su - webadmin
 sudo apt-get update
 sudo apt-get install apache2
