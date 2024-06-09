de967b4c345ec600ef8da4f8a65d80b7239a1656
omarshaban32 <54452908+omarshaban32@users.noreply.github.com>
2024-06-09 03:01:21 +0300

    Update ping.sh

--- day-1/lab-3/ping.sh
+++ day-1/lab-3/ping.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
+# ping to my network 192.168.1.[0:255]
 # Loop through the range 0-255 for the last octet of the IP address
 for i in {0..255}
 do
   # Ping the current IP address with a timeout of 1 second
-  if ping -c 1 -W 1 192.168.226.$i &> /dev/null
+  if ping -c 1 -W 1 192.168.1.$i &> /dev/null
   then
-    echo "Server 192.168.226.$i is up and running"
+    echo "Server 192.168.1.$i is up and running"
   else
-    echo "Server 192.168.226.$i is unreachable"
+    echo "Server 192.168.1.$i is unreachable"
   fi
 done
 
